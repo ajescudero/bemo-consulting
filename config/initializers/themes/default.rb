f4ef5bd0ffcabe9a9c5502de946f99dc8221ad43
Aldo Escudero <aldoescudero@MacBook-Pro-de-Aldo.local>
2020-05-07 13:26:00 -0300

    Initial commit

---
+++ config/initializers/themes/default.rb
@@ -0,0 +1,44 @@
+::Spina::Theme.register do |theme|
+
+  theme.name = 'default'
+  theme.title = 'Default Theme'
+
+  theme.page_parts = [{
+    name:           'text',
+    title:          'Text',
+    partable_type:  'Spina::Text'
+  }, {
+    name:           'image',
+    title:          "Image",
+    partable_type:  'Spina::Image'
+  }]
+
+  theme.view_templates = [{
+    name:       'homepage',
+    title:      'Homepage',
+    page_parts: ['text', 'image']
+  }, {
+    name: 'show',
+    title:        'Default',
+    description:  'A simple page',
+    usage:        'Use for your content',
+    page_parts:   ['text']
+  }]
+
+  theme.custom_pages = [{
+    name:           'homepage',
+    title:          'Homepage',
+    deletable:      false,
+    view_template:  'homepage'
+  }]
+
+  theme.navigations = [{
+    name: 'mobile',
+    label: 'Mobile'
+  }, {
+    name: 'main',
+    label: 'Main navigation',
+    auto_add_pages: true
+  }]
+
+end
